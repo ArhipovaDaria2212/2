b009e0241c4211e2a82888ab03dd1646d5c89538
Arhipova Daria <daria221220042017@gmail.com>
2022-02-14 12:25:30 +0700

    квадратное уравнение

---
+++ index1.js
@@ -0,0 +1,17 @@
+console.log('ghjfhgfjgj')
+let a = 2
+let b = 1
+let c = -2
+let x1, x2
+
+let D = b*b - 4*a*c
+
+if (D>0) {
+    x1 = (-b+ Math.sqrt(D)) / 2*a
+    x2 = (-b- Math.sqrt(D)) / 2*a
+    console.log(`x1 = ${x1}`)
+    console.log(`x1 = ${x2}`)
+}
+else {
+    console.log('Действительно корней нет')
+}
