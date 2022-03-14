73f72b95d950d7457475b5c0a64807c507d2ced3
Arhipova Daria <daria221220042017@gmail.com>
2022-03-14 11:13:17 +0700

    revert

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-    $("div#head1").draggable({helper:"clone"});
+    $("div#head1").draggable({revert:true});
     $("div#head2").draggable({helper:function(event){return $("<div>Я элемент помощник.</div>")}});
 });
