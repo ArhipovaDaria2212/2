3a9de82077f06bec840f77ca0c3a15e4a682afb8
Arhipova Daria <daria221220042017@gmail.com>
2022-03-14 11:24:41 +0700

    accept

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-$(document).ready(function() {
-    $(document).ready(function(){
-        $("#drag1").draggable({stack:"#drop1"});
-        $("#drop1").droppable({
-           over:function(){
-              $("#drag1").css("background-color","#d7fa99");
-              $("#drop1").css("background-color","#d7fa99");
-           },
-           drop:function(){
-              $("#drag1").css("display","none");
-              $("#drop1").css("background-color","#c4f66f");
-              $("#drop1").html("Перемещение завершено успешно.");
-              alert("Вы перетащили элемент с id=drag1 в область с id=drop1.");
-           }
-        });
-     });
-});
+$(document).ready(function(){
+    $("#drag1").draggable({stack:"#drop1"});
+    $("#drag2").draggable({stack:"#drop1",revert:true});
+    $("#drop1").droppable({
+       accept:"#drag1",
+       over:function(){
+          $("#drag1").css("background-color","#d7fa99");
+          $("#drop1").css("background-color","#d7fa99");
+       },
+       drop:function(){
+          $("#drag1").css("display","none");
+          $("#drop1").css("background-color","#c4f66f");
+          $("#drop1").html("Перемещение завершено успешно.");
+       }
+    });
+ });
