277eb455d1721c84d25beca7af66262d70f69d23
Arhipova Daria <daria221220042017@gmail.com>
2022-03-14 11:22:42 +0700

    drop$over

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
-    $("div#head1").draggable({revert:true});
-    $("div#head2").draggable({helper:function(event){return $("<div>Я элемент помощник.</div>")}});
+    $(document).ready(function(){
+        $("#drag1").draggable({stack:"#drop1"});
+        $("#drop1").droppable({
+           over:function(){
+              $("#drag1").css("background-color","#d7fa99");
+              $("#drop1").css("background-color","#d7fa99");
+           },
+           drop:function(){
+              $("#drag1").css("display","none");
+              $("#drop1").css("background-color","#c4f66f");
+              $("#drop1").html("Перемещение завершено успешно.");
+              alert("Вы перетащили элемент с id=drag1 в область с id=drop1.");
+           }
+        });
+     });
 });
