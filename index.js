6e5e8d25ca1034ea10fc90618ed975a3731254c3
Arhipova Daria <daria221220042017@gmail.com>
2022-03-14 11:11:13 +0700

    helper

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
-    $("div#head1").draggable({cursor:"help"});
+    $("div#head1").draggable({helper:"clone"});
+    $("div#head2").draggable({helper:function(event){return $("<div>Я элемент помощник.</div>")}});
 });
